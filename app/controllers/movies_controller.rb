d3f8c33d303165f3effd159bb5162bb4ec1badca
Santiaago <santiago.ariassar@gmail.com>
2012-03-18 16:15:52 +0100

    part 41

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,7 +34,7 @@ class MoviesController < ApplicationController
     
     if params["ratings"]
       @ratings= params["ratings"]
-      @movies = Movies.find :all, :ratings => @ratings
+      @movies = Movies.find :all, :order => 'title'#:ratings => @ratings
     else
       @ratings = {}
       @all_ratings.each do |rating|
