c01c0c6c264bfe79f2f2cb3380365546e3aef5aa
Santiaago <santiago.ariassar@gmail.com>
2012-03-18 17:17:46 +0100

    part 41

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,65 +17,47 @@ class MoviesController < ApplicationController
     redirect = false
     flash[:title_color] = ''
     flash[:release_date_color] = ''
-    @ratings = params["ratings"]
-    @all_ratings = Movie.all_ratings
-    
     
     if params[:sort] == 'title'
-      #@movies = Movie.find :all, :order => 'title'
-      @movies = Movies.order(params[:sort])
+      
       flash[:title_color] = 'hilite'
-      #@sort_by = params[:sort]
+      @sort_by = params[:sort]
+      
     elsif params[:sort] == 'release_date'
-      #@movies = Movie.find :all, :order => 'release_date'
-      @movies = Movies.order(params[:sort])
+      
       flash[:release_date_color] = 'hilite'
-      #@sort_by = params[:sort]
+      @sort_by = params[:sort]
+      
     else
-      #@movies = Movie.all
-      #@sort_by = :id
-      #redirect = true
-      @movies = []
-      if @ratings
-        @ratings.each do |rating|
-          Movie.getMoviesWithRating(rating).each do |movie|
-            @movie << movie
-          end
-        end
-        return @movies
-      else
-        @movies = []
-      end
-      return @movies
+      @sort_by = :id
+      redirect = true
     end
-    
-    #if params["ratings"]
-    #  @ratings= params["ratings"]
-      #@movies = Movies.find(:all, :conditions => @ratings)#:ratings => @ratings
-    #else
-    #  @ratings = {}
-    #  @all_ratings.each do |rating|
-    #    @ratings[rating]="yes"
-    #  end
-    #      redirect = true
-    #end
-    #if redirect
-    #    @movies = Movie#.all
-        # redirect_to movies_path(:order=>@sort_by)#,:ratings=>@ratings)
-    #end
-    
-    #all_movies = Movie.order(@sort_by)
-    #@movies = []
-    #all_movies.each do |movie|
-    #  if @ratings.keys.include?(movie["rating"])
-    #    @movies << movie
-    #  end
-    #end
-    
+    if params["ratings"]
+      @ratings = params["ratings"]
+    else
+      @ratings = {}
+      @all_ratings.each do |rating|
+        @ratings[rating] = "yes"
+      end
+      redirect = true
+    end
+    if redirect
+      redirect_to movies_path(:sort=>@sort_by,:ratings=>@ratings)
+    end
+    all_movies = Movies.order(@sort_by)
+    @movies = []
+    all_movies.each do |movie|
+      if @ratings.key.include?(movie["rating"])
+        @movies << movie
+      end
+    end
+    flash[:sort] = @sort_by
+    flash[:ratings] = @ratings
   end
 
   def new
     # default: render 'new' template
+    @all_ratings = Movie.find(:all,:select=>"rating", :group => "rating").map(&:rating)
   end
 
   def create
